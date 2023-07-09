674b82c6bd33b2930450c2c3a7259c6e6cc59c21
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-08 23:51:45 -0600

    project done

---
+++ introduction-to-java/src/DataTypes.java
@@ -0,0 +1,107 @@
+import java.util.Arrays;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+//        int varInt = 21;
+
+//        System.out.println("int value is : " + varInt);
+
+//        int varInt = 2147483647;
+//        System.out.println("The int value is : " + varInt);
+
+//        int newVarInt = 214_748_3647;
+//        System.out.println("The new int value is : " + newVarInt);
+
+//        int newVarInt = 214_748_3647_891;
+//        System.out.println("The new int value is : " + newVarInt);
+
+//        long varLong = 214_748_3647_891L;
+
+//        System.out.println("The long value is : " + varLong);
+
+//        float varFloat = 123.45F;
+//        System.out.println("The float value is : " + varFloat);
+
+//        double varDouble = 123.45D;
+//        System.out.println("The double value is : " + varDouble);
+
+//        float varFloat = 10e3f;
+//        System.out.println("The float value is : " + varFloat);
+
+//        double varDouble = 10E-4d;
+//        System.out.println("The double value is : " + varDouble);
+
+//        byte varByte = 30;
+//        System.out.println("byte value is : " + varByte);
+
+//        byte negativeByte = -100;
+//        System.out.println("The negative byte value is :" + negativeByte);
+
+//        byte newVarByte = 127;
+//        System.out.println("The new byte value is :" + newVarByte);
+
+//        short varShort = 32768;
+//        System.out.println("short value is : " + varShort);
+
+//        short newVarShort = -32767;
+//        System.out.println("New short value is : " + newVarShort);
+
+//        char letter = 'A';
+
+//        System.out.println("Letter is : " + letter);
+
+//        char unicodeVal = '\u0052';
+
+//        System.out.println("The unicode letter is : " + unicodeVal);
+
+//        boolean isJavaFun = true;
+//        System.out.println("Java is fun! : " + isJavaFun);
+
+//        boolean isTheSkyOrange = false;
+//        System.out.println("The sky is orange. : " + isTheSkyOrange);
+
+//        int num1 = 5;
+//        int num2 = 10;
+//        boolean result = num2 > num1;
+
+//        System.out.println("Is num2 greater than num1 ? " + result);
+//        System.out.println("Is num1 greater than num2 ? " + (num1 > num2) );
+//        System.out.println("Is num1 less than num2 ? " + (num1 < num2) );
+
+//        String name = "Fiona";
+//        String name = new String("Fiona");
+
+//        System.out.println("The name is : " +  name);
+
+//        String message = "Fiona\b says Hello";
+
+//        System.out.println(message);
+
+        String[] carBrands = { "Volvo", "BMW", "Ford", "Mazda" };
+
+//        System.out.println("Car brands : " + carBrands);
+
+//        String carBrandsString = Arrays.toString(carBrands);
+
+//        System.out.println("The string of car brands : " + carBrandsString);
+
+//        System.out.println("Number of car brands : " + carBrands.length);
+
+//        System.out.println("carBrands[0] : " + carBrands[0]);
+//        System.out.println("carBrands[1] : " + carBrands[1]);
+//        System.out.println("carBrands[2] : " + carBrands[2]);
+//        System.out.println("carBrands[3] : " + carBrands[3]);
+//        carBrands[0] = "Kia";
+
+//        String carString = Arrays.toString(carBrands);
+
+//        System.out.println("carBrands[0] after the change : " + carBrands[0]);
+
+//        System.out.println("List of car brands : " + carString);
+
+        System.out.println("carBrands[4] : " + carBrands[4]);
+
+    }
+}
---
+++ introduction-to-java/src/MyFirstProgram.java
@@ -0,0 +1,56 @@
+public class MyFirstProgram {
+//    public static int staticVar = 20;
+    public static String name = "Smith";
+
+    public static void main(String[] args) {
+
+//        int number = 20;
+
+//        System.out.println(number);
+
+//        int varInt = 15;
+
+//        int varInt;
+
+//        varInt = 100;
+//        varInt = 50;
+
+//        System.out.println(varInt);
+
+//        final int maxRating = 10;
+//        maxRating = 100;
+
+//        System.out.println(maxRating);
+
+//        String varString = "This is a string";
+
+//        System.out.format(varString);
+
+//        System.out.println("The string value : " + varString);
+
+//        int varInt = 100;
+
+//        System.out.println("The integer value is : " + varInt);
+
+//        System.out.format("The integer %d is the value of the variable.", varInt);
+
+//        String message = "Good Morning!!!";
+
+//        System.out.format("The message %s has been received", message);
+
+//        int x = 5, y = 6, z = 50;
+
+//        System.out.println("X value is : " + x );
+//        System.out.println("Y value is : " + y );
+//        System.out.println("X value is : " + z );
+
+//        int localVar = 10;
+
+//        System.out.println("Static variable value is : " + staticVar);
+//        System.out.println("Local variable value is : " + localVar);
+
+        int salary = 50000;
+
+        System.out.println(name + " Salary is : " + salary);
+    }
+}
